a066170637bc0b083757d29659801a8d154b831f
Chloe <gtalk.with.nani@gmail.com>
2023-08-09 16:42:12 +0900

    main Add Militery-service, language, award, activity

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import Head from 'next/head'
 import axios from 'axios';
 import { useAuth } from '@/components/hooks/authContext'
 import LinkButton from '@/components/hooks/linkButton';
@@ -44,7 +42,6 @@ export default function Home() {
 
   return (
     <Container className='css-0'>
-      {console.log('resumeData: ', resumeData)}
       {loggedIn ? (
         <>
           <h1>이력서 정보</h1>
@@ -84,6 +81,48 @@ export default function Home() {
                 {resumeData.disability ? resumeData.disability : <strong>장애대상 여부를 입력하지 않아 프린트 할 때 공개되지 않습니다</strong>}
               </dd>
             </dl>
+          </section>
+          <section>
+            <h2>병역사항</h2>
+            {resumeData.military_services.length > 0 ? (
+              <dl className='array'>
+                {resumeData.military_services[0] &&
+                  <dl>
+                    <dt>병역여부</dt>
+                    <dd>{resumeData.military_services[0].military_stats ? '군필' : '미필'}</dd>
+                    <dt>이력서 병역여부 공개</dt>
+                    <dd>{resumeData.military_services[0].military_show ? '공개' : '공개 안함'}</dd>
+                    {resumeData.military_services[0].military_stats === false ?
+                      <>
+                        <dt>면제 사유</dt>
+                        <dd>{resumeData.military_services[0].conscription_exemption ? resumeData.military_services[0].conscription_exemption : '면제사유 미입력'}</dd>
+                      </>
+                      :
+                      <>
+                        <dt>군별</dt>
+                        <dd>{resumeData.military_services[0].military_group ? resumeData.military_services[0].military_group : '군별 미입력'}</dd>
+                        <dt>병과</dt>
+                        <dd>{resumeData.military_services[0].branch ? resumeData.military_services[0].branch : '병과 미입력'}</dd>
+                        <dt>계급</dt>
+                        <dd>{resumeData.military_services[0].rank ? resumeData.military_services[0].rank : '계급 미입력'}</dd>
+                        <dt>병역</dt>
+                        <dd>{resumeData.military_services[0].discharge ? resumeData.military_services[0].discharge : '병역 미입력'}</dd>
+                        <dt>복무 시작일</dt>
+                        <dd>{resumeData.military_services[0].start_date ? resumeData.military_services[0].start_date : '복무 시작일 미입력'}</dd>
+                        {resumeData.military_services[0].end_date &&
+                          <>
+                            <dt>전역/제대일</dt>
+                            <dd>{resumeData.military_services[0].end_date}</dd>
+                          </>
+                        }
+                      </>
+                    }
+                  </dl>
+                }
+              </dl>
+            ) : (
+              <p>아직 병역사항을 등록하지 않으셨습니다</p>
+            )}
           </section>
           <section>
             <h2>학력사항</h2>
@@ -154,6 +193,46 @@ export default function Home() {
             )}
           </section>
           <section>
+            <h2>외국어능력</h2>
+            {resumeData?.languages?.length > 0 ? (
+              <dl className='array'>
+                {resumeData?.languages?.map((language) => (
+                  <Fragment key={language.id}>
+                    <dt>외국어명</dt>
+                    <dd>{language.lang_name}</dd>
+                    <dt>시험명</dt>
+                    <dd>{language.exam_name}</dd>
+                    <dt>점수</dt>
+                    <dd>{language.point}</dd>
+                  </Fragment>
+                ))}
+              </dl>
+            ) : (
+              <p>아직 외국어능력을 등록하지 않으셨습니다</p>
+            )}
+          </section>
+          <section>
+            <h2>수상기록</h2>
+            {resumeData?.awards?.length > 0 ? (
+              <dl className='array'>
+                {resumeData?.awards?.map((award) => (
+                  <Fragment key={award.id}>
+                    <dt>수상명</dt>
+                    <dd>{award.award_name}</dd>
+                    <dt>수상내용</dt>
+                    <dd>{award.description}</dd>
+                    <dt>발행기관</dt>
+                    <dd>{award.organization}</dd>
+                    <dt>취득일</dt>
+                    <dd>{award.issue_date}</dd>
+                  </Fragment>
+                ))}
+              </dl>
+            ) : (
+              <p>아직 수상기록을 등록하지 않으셨습니다</p>
+            )}
+          </section>
+          <section>
             <h2>보유기술</h2>
             {resumeData?.skills?.length > 0 ? (
               <dl className='array'>
@@ -194,6 +273,47 @@ export default function Home() {
             </dl>
           </section>
           <section>
+            <h2>대외활동</h2>
+            {resumeData?.activities?.length > 0 ? (
+              <dl className='array'>
+                {resumeData?.activities?.map((activity) => (
+                  <Fragment key={activity.id}>
+                    <dt>기관명</dt>
+                    <dd>{activity.organization}</dd>
+                    <dt>직책/담당업무</dt>
+                    <dd>{activity.position}</dd>
+                    <dt>활동내용</dt>
+                    <dd>{activity.description}</dd>
+                    <dt>활동구분</dt>
+                    <dd>{activity.classification}</dd>
+                    <dt>시작일시</dt>
+                    <dd>{activity.start_date}</dd>
+                    {activity.start_time &&
+                      <>
+                        <dt>시작시간</dt>
+                        <dd>{activity.start_time}</dd>
+                      </>
+                    }
+                    {activity.end_date &&
+                      <>
+                        <dt>종료일시</dt>
+                        <dd>{activity.end_date}</dd>
+                      </>
+                    }
+                    {activity.end_time &&
+                      <>
+                        <dt>종료시간</dt>
+                        <dd>{activity.end_time}</dd>
+                      </>
+                    }
+                  </Fragment>
+                ))}
+              </dl>
+            ) : (
+              <p>아직 대외활동을 등록하지 않으셨습니다</p>
+            )}
+          </section>
+          <section>
             <h2>경력사항</h2>
             {resumeData?.careers?.length > 0 ? (
               <dl className='array'>
@@ -206,8 +326,17 @@ export default function Home() {
                       <dd>{career.team}</dd>
                       <dt>입사일/합류일</dt>
                       <dd>{career.start_date}</dd>
-                      <dt>퇴사일/만료일</dt>
-                      <dd>{career.end_date}</dd>
+                      {career.end_date ?
+                        <>
+                          <dt>퇴사일/만료일</dt>
+                          <dd>{career.end_date}</dd>
+                        </>
+                        :
+                        <>
+                          <dt>퇴사일/만료일</dt>
+                          <dd>재직 중</dd>
+                        </>
+                      }
                       <dt>직업/일</dt>
                       <dd>{career.occupation}</dd>
                       <dt>역할</dt>
@@ -221,8 +350,17 @@ export default function Home() {
                             <dd>{project.project_name}</dd>
                             <dt>프로젝트 시작일</dt>
                             <dd>{project.start_date}</dd>
-                            <dt>프로젝트 종료</dt>
-                            <dd>{project.end_date}</dd>
+                            {project.end_date ?
+                              <>
+                                <dt>프로젝트 종료</dt>
+                                <dd>{project.end_date}</dd>
+                              </>
+                              :
+                              <>
+                                <dt>프로젝트 종료</dt>
+                                <dd>프로젝트 진행 중</dd>
+                              </>
+                            }
                             <dt>프로젝트 설명</dt>
                             <dd>
                               {project.description ? (
